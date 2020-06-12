c693a87c6872a94cafe8be3b41ec036c66f9c90f
Danny Helton <DHE293@uky.edu>
2020-06-12 04:47:06 -0700

    Update point.js
    point adjustment

--- js/point.js
+++ js/point.js
@@ -1,18 +1,5 @@
 (function () {
 
-    const mapContainer = d3.select('#map')
-
-    const width = mapContainer.node().offsetWidth - 60;
-    const height = mapContainer.node().offsetHeight - 60;
-
-    const svg = mapContainer
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .classed('position-absolute', true)
-        .style('top', 40)
-        .style('left', 30);
-
     const pollutionGeoJson = d3.json('data/air_pollution_data.geojson')
     const countyTopoJson = d3.json('data/counties.topojson')
 
@@ -28,6 +15,19 @@
     }
 
     function drawMap(pollutant, data) {
+
+        const mapContainer = d3.select('#map')
+
+    const width = mapContainer.node().offsetWidth - 60;
+    const height = mapContainer.node().offsetHeight - 60;
+
+    const svg = mapContainer
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .classed('position-absolute', true)
+        .style('top', 40)
+        .style('left', 30);
 
         svg.selectAll('*').remove()
 
@@ -46,11 +46,10 @@
 
         const max = d3.max(myArray)
         const min = d3.min(myArray)
-        console.log(myArray, min, max)
-
-        const color = d3.scaleQuantize([min, max], d3.schemePurples[9]);
 
         const radius = d3.scaleSqrt().domain([0, 1e6]).range([1, 9]);
+
+        const color = d3.scaleQuantize([min, max], d3.schemePurples[9])
 
         const projection = d3.geoConicEquidistant()
             .center([0, 40])
@@ -69,20 +68,18 @@
             .attr('class', 'county');
 
             const pollution = svg.append('g')
-            .selectAll('circle')
-            .data(pollutionData)
-            .join('circle')
-            .attr('cx', d => {
-              d.position = projection([d.Longitude, d.Latitude]);
-              return d.position[0];
+            .selectAll('path')
+            .data(pollutionData.features)
+            .enter()
+            .append('path')
+            .attr("fill", d => {
+                return color(d.properties[pollutant]);
             })
-            .attr('cy', d => {
-              return d.position[1];
-            })
-            .attr('r', d => {
-                return radius(+d.properties[pollutant]);
-              })
-            .attr('class', 'facility'); 
+            .attr('d', path.pointRadius(5));
+            
+            // .attr('d', path.pointRadius([function (d) {
+            //    return radius(d.properties[pollutant]);
+            // }]))
 
         const tooltip = d3.select('.container-fluid').append('div')
             .attr('class', 'my-tooltip bg-warning text-white py-1 px-2 rounded position-absolute invisible');
@@ -95,11 +92,41 @@
 
         pollution.on('mousemove', (d, i, nodes) => {
             d3.select(nodes[i]).classed('hover', true).raise();
-            tooltip.classed('invisible', false).html(`${d.features.State_Name} County<br>${d.features[pollutant]} parts per billion`)
+            tooltip.classed('invisible', false).html(`${d.properties.County_Name} County<br>${d.properties[pollutant]} parts per billion`)
         })
             .on('mouseout', (d, i, nodes) => {
                 d3.select(nodes[i]).classed('hover', false)
                 tooltip.classed('invisible', true)
             });
+
+            drawLegend(svg, width, height, radius)
     }
+
+    function drawLegend(svg, width, height, radius) {
+
+        const legend = svg.append('g')
+          .attr('dy', '1.3em')
+          .attr('class', 'legend')
+          .attr('transform', 'translate(' + (width - 40) + ',' + (height - 20) + ')')
+          .selectAll('g')
+          .data([5e6, 2e7])
+          .join('g');
+  
+        legend.append('circle')
+          .attr('cy', d => {
+            return -radius(d);
+          })
+          .attr('r', radius);
+  
+        legend.append('text')
+          .attr('y', d => {
+            return -2 * radius(d);
+          })
+          .attr('dy', '1.3em')
+          .text(d3.format('.1s'));
+  
+        legend.append('text')
+          .attr('y', 16)
+          .text('metric tons');
+      }
 })();
